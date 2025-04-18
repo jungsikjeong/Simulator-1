c39f3d091e60f9eef9389ca1e9eb02ac7c50d001
JungSik Jeong <wndtlr1024@gmail.com>
2025-04-18 17:20:12 +0900

    mock img 및 AnimatePresence 추기

--- src/routes/_app/index.tsx
+++ src/routes/_app/index.tsx
@@ -3,6 +3,7 @@ import type { SceneKey } from '@/modules';
 import { createFileRoute } from '@tanstack/react-router';
 import { useCallback, useState } from 'react';
 import StartScene from './-components/StartScene';
+import { AnimatePresence } from 'framer-motion';
 
 export const Route = createFileRoute('/_app/')({
   component: App,
@@ -26,24 +27,9 @@ export default function App() {
     return <div>Scene not found: {scene}</div>;
   }
 
-  return <SceneComponent onSceneChange={handleSceneChange} />;
-
-  // return (
-  //   <>
-  //     <h1 className='text-2xl font-bold mb-4'>{current.title}</h1>
-  //     <p className='whitespace-pre-line mb-6'>{current.text}</p>
-
-  //     <div className='space-y-2 flex flex-col gap-2'>
-  //       {current.choices.map((choice, i) => (
-  //         <button
-  //           key={i}
-  //           onClick={() => setScene(choice.next as SceneKey)}
-  //           className='bg-black text-white px-4 py-2 rounded-2xl shadow-md hover:bg-gray-800'
-  //         >
-  //           {choice.label}
-  //         </button>
-  //       ))}
-  //     </div>
-  //   </>
-  // );
+  return (
+    <AnimatePresence>
+      <SceneComponent onSceneChange={handleSceneChange} />;
+    </AnimatePresence>
+  )
 }
