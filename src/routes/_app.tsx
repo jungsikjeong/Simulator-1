368b886dd3abda639e0463edf37be60e6bc02503
jungsikJeong <wndtlr1024@gmail.com>
2025-04-19 14:30:27 +0900

    루트 레이아웃 css 변경

--- src/routes/_app.tsx
+++ src/routes/_app.tsx
@@ -1,15 +1,15 @@
-import { createFileRoute, Outlet } from '@tanstack/react-router';
+import { createFileRoute, Outlet } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/_app')({
   component: RouteComponent,
-});
+})
 
 function RouteComponent() {
   return (
-    <div className='min-h-screen flex flex-col items-center justify-center bg-yellow-50 text-center p-6'>
+    <div className="min-h-screen flex flex-col items-center justify-center max-w-md md:max-w-lg lg:max-w-xl mx-auto text-center relative">
       <main>
         <Outlet />
       </main>
     </div>
-  );
+  )
 }
