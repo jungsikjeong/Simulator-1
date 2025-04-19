e1b34cef129a21afe34f09d4108adb943cacdec7
jungsikJeong <wndtlr1024@gmail.com>
2025-04-19 14:30:18 +0900

    refactor: 씬 매핑 객체 업데이트

--- src/lib/sceneMap.ts
+++ src/lib/sceneMap.ts
@@ -1,15 +1,16 @@
-import type { SceneKey } from '@/modules';
-import StartScene from '../routes/_app/-components/StartScene';
+import type { SceneKey } from '@/modules'
+import SceneA from '@/routes/_app/-components/part-1/SceneA'
+import StartScene from '../routes/_app/-components/StartScene'
+import type React from 'react'
 
-// 추후 Record로 바꿀것
-// export const SceneComponentMap: Record<
-//   SceneKey,
-//   React.FC<{ onSceneChange: (scene: SceneKey) => void }>
-// > = {
-//   start: StartScene,
-// };
-export const sceneComponentMap: Partial<
-  Record<SceneKey, React.FC<{ onSceneChange: (scene: SceneKey) => void }>>
-> = {
+type SceneProps = {
+  onSceneChange: (scene: SceneKey) => void
+}
+
+type SceneComponent = (props: SceneProps) => React.JSX.Element
+
+// 각 씬 키에 해당하는 컴포넌트를 매핑하는 객체
+export const SceneComponentMap: Record<SceneKey, SceneComponent> = {
   start: StartScene,
-};
+  part1: SceneA,
+}
