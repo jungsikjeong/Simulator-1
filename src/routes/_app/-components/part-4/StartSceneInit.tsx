a4bd285b929101bc1fc02272193b2fd597408f9f
Pear-HyunJI <ponyo3764@gmail.com>
2025-05-22 15:38:52 +0900

    배포에러

--- src/routes/_app/-components/part-4/StartSceneInit.tsx
+++
@@ -1,6 +0,0 @@
-<motion.div
-    className="absolute bottom-28 md:bottom-32 w-full text-center"
-    initial={{ y: -20, opacity: 0 }}
-    animate={{ y: 0, opacity: 1 }}
-    transition={{ duration: 1, ease: "easeOut" }}
-> 
