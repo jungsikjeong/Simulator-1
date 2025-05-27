d1d56313867f52d9b1a38837a518b7e047b97e4c
JungSik Jeong <wndtlr1024@gmail.com>
2025-05-27 15:01:09 +0900

    짐빔 캔 나오는속도 조금 늦춤 + 짐빔 캔 사이즈 모바일 축소

--- src/routes/_app/-components/part-1/SceneB/JimBeamCan.tsx
+++ src/routes/_app/-components/part-1/SceneB/JimBeamCan.tsx
@@ -15,13 +15,13 @@ const JimBeamCan: React.FC<JimBeamCanProps> = React.memo(({ show }) => {
             alt="Jim Beam Can"
             initial={{ y: 200, opacity: 0 }}
             animate={{ y: 0, opacity: 1 }}
-            transition={{ type: 'tween', duration: 0.8, ease: 'easeInOut' }}
+            transition={{ type: 'tween', duration: 1.8, ease: 'easeInOut' }}
             style={{
                 position: 'absolute',
                 left: isMobile ? '3%' : '5%',
                 bottom: isMobile ? -35 : -65,
                 transform: 'translateX(-50%)',
-                width: isMobile ? 120 : 150,
+                width: isMobile ? 80 : 150,
                 zIndex: 30,
                 pointerEvents: 'none',
                 willChange: 'transform, opacity',
